b070b3c4e25f082f94dad5bfcb4935b899d39b6f
Sabrine Krichen <s.krichen@instadeep.com>
2023-12-25 19:45:00 +0100

    test: check the value class operations

---
+++ test/test_value.py
@@ -0,0 +1,46 @@
+"""Test value class functionalities."""
+import torch
+
+from micrograd.value import Value
+
+
+def test_value_operations() -> None:
+    """
+    Check the value class operations.
+    """
+    # inputs x1_value, x2_value
+    x1_value = Value(2.0, label="x1")
+    x2_value = Value(0.0, label="x2")
+    # inputs w1_value, w2_value
+    w1_value = Value(-3.0, label="w1")
+    w2_value = Value(1.0, label="w2")
+    # bias of the neuron
+    b_value = Value(6.0, label="b")
+
+    x1w1_value = x1_value*w1_value
+    x2w2_value = x2_value*w2_value
+    x1w1x2w2_value = x1w1_value + x2w2_value
+    n_value = x1w1x2w2_value + b_value
+    o_value = n_value.tanh() 
+    o_value.backward()
+
+    # inputs x1_torch, x2_torch
+    x1_torch = torch.tensor(2.0, requires_grad=True)
+    x2_torch = torch.tensor(0.0, requires_grad=True)
+    # inputs w1_torch, w2_torch
+    w1_torch = torch.tensor(-3.0, requires_grad=True)
+    w2_torch = torch.tensor(1.0, requires_grad=True)
+    # bias of the neuron
+    b_torch = torch.tensor(6.0, requires_grad=True)
+
+    x1w1_torch = x1_torch * w1_torch 
+    x2w2_torch = x2_torch * w2_torch
+    x1w1x2w2_torch = x1w1_torch + x2w2_torch
+    n_torch = x1w1x2w2_torch + b_torch
+    o_torch = torch.tanh(n_torch)
+    o_torch.backward()
+
+    assert x1_torch.grad == x1_value.grad
+    assert o_value.data == o_torch.item()
+
+
